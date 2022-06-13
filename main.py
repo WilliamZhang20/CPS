de097e66cd78dccd74d58c196cd4006c2b47da9e
Jonathan Yuan <jy90023@gmail.com>
2022-06-12 20:29:28 -0400

    retrieve user solved problems

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import bs4
 import os
 from dotenv import load_dotenv
 
@@ -24,9 +25,46 @@ def login(username, password):
     return s
 
 
+def solved_problems(username, session):
+
+    response = session.get(f'https://dmoj.ca/api/user/info/{username}').json()
+    problem_list = response['solved_problems']
+
+    return problem_list
+
+
+def best_submission(problem_code, session):
+
+    page_number = 1
+
+    while True:
+        response = session.get(
+            f'https://dmoj.ca/problem/{problem_code}/rank/{page_number}')
+
+        if response.status_code == 404:
+            break
+
+        soup = bs4.BeautifulSoup(response.text, 'lxml')
+        submission = soup.find('a', string=DMOJ_USERNAME)
+
+        if(submission):
+            print(submission, page_number)
+            return
+
+        page_number += 1
+
+
+def process_submission(submission_id, session):
+    response = session.get(f'https://dmoj.ca/src/{submission_id}')
+    soup = bs4.BeautifulSoup(response.text, 'lxml')
+    source_code = soup.find('code').text
+
+
 load_dotenv()
 
 DMOJ_USERNAME = os.getenv('DMOJ_USERNAME')
 DMOJ_PASSWORD = os.getenv('DMOJ_PASSWORD')
 
 session = login(DMOJ_USERNAME, DMOJ_PASSWORD)
+
+problem_list = solved_problems(DMOJ_USERNAME, session)
