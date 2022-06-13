e18dca15c74f5cd72c706a3fd40de740ded288da
Jonathan Yuan <jy90023@gmail.com>
2022-06-12 21:53:12 -0400

    extract source code from submissions and write into new files

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from venv import create
 import requests
 import bs4
+import itertools
 import os
 from dotenv import load_dotenv
 
 
 def login(username, password):
 
+    # a session object will persist all cookies across requests
     s = requests.Session()
     r = s.get('https://dmoj.ca/accounts/login/?next=/user')
     csrf_token = r.cookies['csrftoken']
@@ -33,7 +36,7 @@ def solved_problems(username, session):
     return problem_list
 
 
-def best_submission(problem_code, session):
+def best_submission(username, problem_code, session):
 
     page_number = 1
 
@@ -45,11 +48,16 @@ def best_submission(problem_code, session):
             break
 
         soup = bs4.BeautifulSoup(response.text, 'lxml')
-        submission = soup.find('a', string=DMOJ_USERNAME)
+        submission = soup.find('a', string=username)
 
         if(submission):
-            print(submission, page_number)
-            return
+            # submission.parents is a generator containing a sequential order of all the parents of the submission link. The parent div containing the submission id is 5 levels above the submission link.
+            # islice will return a new generator from index 4 to the end
+            # calling next() on the sliced iterator will return the links 5th parent, as it is now the first element to be yielded
+
+            submission_id = next(itertools.islice(
+                submission.parents, 4, None))['id']
+            return submission_id
 
         page_number += 1
 
@@ -58,6 +66,13 @@ def process_submission(submission_id, session):
     response = session.get(f'https://dmoj.ca/src/{submission_id}')
     soup = bs4.BeautifulSoup(response.text, 'lxml')
     source_code = soup.find('code').text
+    return source_code
+
+
+def create_file(problem_id, source_code):
+    f = open(f'{problem_id}.cpp', 'w+')
+    f.write(source_code)
+    f.close()
 
 
 load_dotenv()
@@ -68,3 +83,9 @@ DMOJ_PASSWORD = os.getenv('DMOJ_PASSWORD')
 session = login(DMOJ_USERNAME, DMOJ_PASSWORD)
 
 problem_list = solved_problems(DMOJ_USERNAME, session)
+
+
+submission_id = best_submission(DMOJ_USERNAME, 'coci14c1p6', session)
+source_code = process_submission(submission_id, session)
+
+create_file('coci14c1p6', source_code)
