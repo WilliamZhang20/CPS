9ad8c7b712dfb8040e51c1da97884df53a575318
Jonathan Yuan <jy90023@gmail.com>
2022-06-13 13:42:06 -0400

    iterate through solved problem codes and write their source codes to their own files

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from venv import create
+import os
+import time
+import itertools
 import requests
 import bs4
-import itertools
-import os
 from dotenv import load_dotenv
 
 
@@ -30,8 +30,8 @@ def login(username, password):
 
 def solved_problems(username, session):
 
-    response = session.get(f'https://dmoj.ca/api/user/info/{username}').json()
-    problem_list = response['solved_problems']
+    response = session.get(f'https://dmoj.ca/api/v2/user/{username}').json()
+    problem_list = response['data']['object']['solved_problems']
 
     return problem_list
 
@@ -51,7 +51,7 @@ def best_submission(username, problem_code, session):
         submission = soup.find('a', string=username)
 
         if(submission):
-            # submission.parents is a generator containing a sequential order of all the parents of the submission link. The parent div containing the submission id is 5 levels above the submission link.
+            # submission.parents is a generator containing a sequential order of all the parents of the submission link. The parent containing the submission id is 5 levels above the submission link.
             # islice will return a new generator from index 4 to the end
             # calling next() on the sliced iterator will return the links 5th parent, as it is now the first element to be yielded
 
@@ -69,23 +69,31 @@ def process_submission(submission_id, session):
     return source_code
 
 
-def create_file(problem_id, source_code):
-    f = open(f'{problem_id}.cpp', 'w+')
+def create_file(problem_code, source_code, directory):
+    f = open(f'{directory}\{problem_code}.cpp', 'w+', encoding='utf-8')
     f.write(source_code)
     f.close()
 
 
+# create DMOJ_USERNAME and DMOJ_PASSWORD environment variables as defined in .env
 load_dotenv()
 
+# load them into the program
 DMOJ_USERNAME = os.getenv('DMOJ_USERNAME')
 DMOJ_PASSWORD = os.getenv('DMOJ_PASSWORD')
+TARGET_DIRECTORY = os.getenv('TARGET_DIRECTORY')
 
 session = login(DMOJ_USERNAME, DMOJ_PASSWORD)
 
 problem_list = solved_problems(DMOJ_USERNAME, session)
 
+for i, problem_code in enumerate(problem_list):
 
-submission_id = best_submission(DMOJ_USERNAME, 'coci14c1p6', session)
-source_code = process_submission(submission_id, session)
+    print(
+        f'creating file {problem_code} [{i + 1}/{len(problem_list)}]', end='\n', flush=True)
 
-create_file('coci14c1p6', source_code)
+    submission_id = best_submission(DMOJ_USERNAME, problem_code, session)
+    source_code = process_submission(submission_id, session)
+    create_file(problem_code, source_code, TARGET_DIRECTORY)
+
+    time.sleep(2)
